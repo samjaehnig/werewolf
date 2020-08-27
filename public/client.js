fae28ce7e56be2387d95c7182f61ccc3ab0dff5e
sjaehnig <sjaehnig@umich.edu>
2020-08-27 09:34:25 -0700

    first commit

---
+++ public/client.js
@@ -0,0 +1,52 @@
+var game_started = false;
+
+var socket = io.connect("http://localhost:5678");
+
+socket.on("connect", function(data) {
+    socket.emit("join");
+});
+
+socket.on("player", function(data) {
+    $("#players").append("<span class='player'>" + data + "</span>");
+});
+
+socket.on("existing_players", function(data) {
+    for(var i = 0; i < data.length; i++) { $("#players").append("<span class='player'>" + data[i][0] + "</span>"); }
+});
+
+socket.on("player_me", function(data) {
+    $("#players").append("<span class='player_me' id='player_me'><b>" + data + " (you)" + "</b></span>");
+    document.getElementById('name').parentNode.removeChild(document.getElementById('name'));
+    document.getElementById('name_submit').parentNode.removeChild(document.getElementById('name_submit'));
+});
+
+socket.on("display_start", function() {
+    $("#start_game").append("<input id='start_submit' type='submit' value='click here to start the game!' />");
+});
+
+socket.on("remove_start", function() {
+    document.getElementById('start_submit').parentNode.removeChild(document.getElementById('start_submit'));
+});
+
+socket.on("role", function(data) {
+    $("#role_div").append("<span id='role_intro'>your role is.... </span><span id='role'>" + data + " </span>");
+});
+
+
+$("#add_player").submit(function() {
+    if(!game_started) {
+        var player_name = $("#name").val();
+        if(player_name != "") { socket.emit("add_player", player_name); }
+        this.reset();
+        return false;
+    } else {
+        //print something to the user given the game has already started
+    }
+});
+
+$("#start_game").submit(function() {
+    game_started = true;
+    socket.emit("starting");
+    this.reset();
+    return false;
+});
---
+++ server.js
@@ -0,0 +1,69 @@
+var express = require("express");
+var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
+let users = [];
+let roles = ["werewolf", "werewolf", "minion", "seer", "robber", "troublemaker", "insomniac", "tanner"];
+let center1, center2, center3;
+
+app.get("/", function(req, res, next) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.use(express.static("public"));
+
+
+io.on("connection", function(client) {
+    
+    // client.on("join", function() {
+    // });
+
+  client.on("add_player", function(data) {
+    // console.log(io.engine.clientsCount);
+    //also emit to just this client any players already in the room!
+    // client.emit("existing_players", users);
+
+    client.emit("player_me", data);
+    client.broadcast.emit("player", data);
+    var user = [data, client.id];
+    users.push(user);
+    if(users.length == 2) { 
+      client.emit("display_start");
+      client.broadcast.emit("display_start");
+    }
+  });
+
+  client.on("starting", function(data) { 
+    client.emit("remove_start");
+    client.broadcast.emit("remove_start");
+    assignroles(); 
+    console.log("delegating roles");
+    for(var i = 0; i < users.length; i++) {
+      if(users[i][1] == client.id) { client.emit("role", users[i][2]); }
+      else { client.broadcast.to(users[i][1]).emit("role", users[i][2]); }
+    }
+  });
+
+
+});
+
+function assignroles() {
+  // console.log("assigning roles");
+  // console.log(users);
+  let temp_roles = roles;
+  let user_index = 0;
+  for(var i = 0; i < users.length; i++) {
+    let role_index = Math.floor(Math.random() * (users.length));
+    console.log(role_index);
+    console.log(temp_roles[role_index]);
+    users[user_index].push(temp_roles[role_index]);
+    console.log(users[user_index]);
+    temp_roles.splice(role_index, 1);
+    user_index++;
+  }
+  center1 = temp_roles[0];
+  center2 = temp_roles[1];
+  center3 = temp_roles[2];
+}
+
+server.listen(5678);
